7d2c6e9cdd82482a0530bbc8c84d1974959185cc
Daniel <danielpellegrini87@gmail.com>
2021-02-04 16:21:29 +0100

    dropdown menu fixed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ var app = new Vue({
   data: {
     chatIndex: 0,
     search: '',
-    isActive: false,
     newMessage: '',
     newMessageArray: [],
     contacts: [
@@ -15,17 +14,20 @@ var app = new Vue({
         messages: [{
             date: '10/01/2020 15:30:55',
             text: 'Hai portato a spasso il cane?',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           },
           {
             date: '10/01/2020 15:50:00',
             text: 'Ricordati di dargli da mangiare',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           },
           {
             date: '10/01/2020 16:15:22',
             text: 'Tutto fatto!',
-            status: 'received'
+            status: 'received',
+            toggle: 'hidden'
           }
         ],
       },
@@ -36,17 +38,20 @@ var app = new Vue({
         messages: [{
             date: '20/03/2020 16:30:00',
             text: 'Ciao come stai?',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           },
           {
             date: '20/03/2020 16:30:55',
             text: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'received',
+            toggle: 'hidden'
           },
           {
             date: '20/03/2020 16:35:00',
             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           }
         ],
       },
@@ -57,17 +62,20 @@ var app = new Vue({
         messages: [{
             date: '28/03/2020 10:10:40',
             text: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'received',
+            toggle: 'hidden'
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            toggle: 'hidden'
           }
         ],
       },
@@ -78,12 +86,14 @@ var app = new Vue({
         messages: [{
             date: '10/01/2020 15:30:55',
             text: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+            status: 'sent',
+            toggle: 'hidden'
           },
           {
             date: '10/01/2020 15:50:00',
             text: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+            status: 'received',
+            toggle: 'hidden'
           }
         ],
       },
@@ -154,6 +164,11 @@ var app = new Vue({
       const lastIndex = messages.length - 1;
       const lastDate = messages[lastIndex].text;
       return lastDate;
+    },
+    toggleDropdown(index){
+      const user = this.contacts[this.chatIndex].messages[index];
+      ( user.toggle == 'hidden' ) ? user.toggle = 'active' : user.toggle = 'hidden';
+
     },
   }
 });
